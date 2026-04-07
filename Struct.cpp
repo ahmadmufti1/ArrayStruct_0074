84f352b13ecbaa1ad62ceed7180fc25725b2835f
ahmadmufti1 <muftiahmad617@gmail.com>
2026-04-07 15:52:24 +0700

    Deklarasi struct

--- Struct.cpp
+++ Struct.cpp
@@ -1,26 +1,9 @@
 #include <iostream>
 using namespace std;
 
+//deklarasi struct
 struct orang{
     string nama;
     string alamat;
     int umur;
 };
-
-int main()
-{
-    //deklarasi obyek dengan struct orang
-    orang mhs;
-    //isi objek
-    cout << "nama mahasiswa:";
-    cin >> mhs.nama;
-    cout << "Alamat Asal:";
-    cin >> mhs.alamat;
-    cout << "umur:";
-    cin >> mhs.umur;
-    cout <<endl;
-    // menampilkan isi objek
-    cout << "Nama : " << mhs.nama<<endl;
-    cout << "alamat : " << mhs.alamat<<endl;
-    cout << "umur : " << mhs.umur<<endl;
-}
