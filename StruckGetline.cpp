1b8f52f895e22453a09b1d1ae630041c8e51b1aa
ahmadmufti1 <muftiahmad617@gmail.com>
2026-04-07 15:58:08 +0700

    deklarasi obyek dengan struct orang, isi objek, dan menampilkan isi objek

--- StruckGetline.cpp
+++ StruckGetline.cpp
@@ -13,3 +13,24 @@ struct orang{
     DetailAlamat alamat;
     int umur;
 };
+
+int main()
+{
+    //deklarasi obyek dengan struct orang
+    orang mhs;
+    //isi objek
+    cout << "nama mahasiswa:";
+    getline( cin, mhs.nama);
+    cout << "Alamat Desa:";
+    cin >> mhs.alamat.desa;
+    getline (cin, mhs.alamat.desa);
+    cin >> mhs.alamat.kota;
+    cout << "umur:";
+    cin >> mhs.umur;
+    cout <<endl;
+    // menampilkan isi objek
+    cout << "Nama Mahasiswa: " << mhs.nama<<endl;
+    cout << "alamat Desa : " << mhs.alamat.desa<<endl;
+    cout << "alamat Kota : " << mhs.alamat.kota<<endl;
+    cout << "umur : " << mhs.umur<<endl;
+}
