e7d2ac08c7421fcf703863041eb9be5fa638b692
ahmadmufti1 <muftiahmad617@gmail.com>
2026-04-07 15:55:20 +0700

    deklarasi struct orang dengan anggota nama, alamat (yang bertipe DetailAlamat), dan umur

--- StruckInStruck.cpp
+++ StruckInStruck.cpp
@@ -6,3 +6,10 @@ struct DetailAlamat{
     string desa;
     string kota;
 };
+
+//deklarasi struct orang dengan anggota nama, alamat (yang bertipe DetailAlamat), dan umur
+struct orang{
+    string nama;
+    DetailAlamat alamat;
+    int umur;
+};
