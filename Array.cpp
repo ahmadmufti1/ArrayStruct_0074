47eba8b2e9a7447b0ff1650c59212bb4e4ff5e89
ahmadmufti1 <muftiahmad617@gmail.com>
2026-04-07 15:50:13 +0700

    operasi matematika , cara menulis atau mengisi arrray dan tampilkan data array dalam satu per satu

--- Array.cpp
+++ Array.cpp
@@ -10,4 +10,19 @@ int main(){
     //cara membaca array dataku
     cout << "data ke - 3 : " << dataku[2]<<endl;
     
+    //operasi matematika 
+    x = dataku [2]*3;
+    //cara menulis atau mengisi arrray
+    dataku[2] = 600;
+    cout << "data ke-3 yang baru : " <<dataku[2]<<endl;
+    
+    // tampilkan data array dalam satu per satu
+    cout << "data ke -1 : "<<datamu[0]<<endl;
+    cout << "data ke -2 : "<<datamu[1]<<endl;
+    cout << "data ke -3 : "<<datamu[2]<<endl;
+    cout << "data ke -4 : "<<datamu[3]<<endl;
+    cout << "data ke -5 : "<<datamu[4]<<endl;
+    
+    cout <<endl;
+
 }
