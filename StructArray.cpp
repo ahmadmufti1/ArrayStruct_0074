68b4bbe9e986b143557339f73f85d4ec2cfa2f3b
ahmadmufti1 <muftiahmad617@gmail.com>
2026-04-07 15:59:36 +0700

    deklarasi obyek dengan struct orang, isi objek dan menampilkan isi objek

--- StructArray.cpp
+++ StructArray.cpp
@@ -13,3 +13,29 @@ struct orang{
     DetailAlamat alamat;
     int umur;
 };
+
+int main()
+{
+    //deklarasi obyek dengan struct orang
+    orang mhs[3];
+    //isi objek
+    for(int i = 0; i <= 2; i++){
+    cout << "nama mahasiswa:";
+    getline( cin, mhs[i].nama);
+    cout << "Alamat Desa:";
+    cin >> mhs[i].alamat.desa;
+    getline (cin, mhs[i].alamat.desa);
+    cin >> mhs[i].alamat.kota;
+    cout << "umur:";
+    cin >> mhs[i].umur;
+    cin.ignore();
+    }
+    cout <<endl;
+    // menampilkan isi objek
+    for(int i = 0;i <= 2; i++){
+    cout << "Nama Mahasiswa: " << mhs[i].nama<<endl;
+    cout << "alamat Desa : " << mhs[i].alamat.desa<<endl;
+    cout << "alamat Kota : " << mhs[i].alamat.kota<<endl;
+    cout << "umur : " << mhs[i].umur<<endl;
+    }
+}
