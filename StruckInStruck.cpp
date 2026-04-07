673a880a9e202f94a29f8e5b6c53b40442327e1c
ahmadmufti1 <muftiahmad617@gmail.com>
2026-04-07 15:54:53 +0700

    deklarasi struct DetailAlamat dengan anggota desa dan kota

--- StruckInStruck.cpp
+++ StruckInStruck.cpp
@@ -1,34 +1,8 @@
 #include <iostream>
 using namespace std;
 
+//deklarasi struct DetailAlamat dengan anggota desa dan kota
 struct DetailAlamat{
     string desa;
     string kota;
 };
-
-struct orang{
-    string nama;
-    DetailAlamat alamat;
-    int umur;
-};
-
-int main()
-{
-    //deklarasi obyek dengan struct orang
-    orang mhs;
-    //isi objek
-    cout << "nama mahasiswa:";
-    cin >> mhs.nama;
-    cout << "Alamat Desa:";
-    cin >> mhs.alamat.desa;
-    cout << "Alamat Kota:";
-    cin >> mhs.alamat.kota;
-    cout << "umur:";
-    cin >> mhs.umur;
-    cout <<endl;
-    // menampilkan isi objek
-    cout << "Nama Mahasiswa: " << mhs.nama<<endl;
-    cout << "alamat Desa : " << mhs.alamat.desa<<endl;
-    cout << "alamat Kota : " << mhs.alamat.kota<<endl;
-    cout << "umur : " << mhs.umur<<endl;
-}
